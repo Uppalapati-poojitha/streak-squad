46e0835c224d952eeb8716441ce8b8903d35c9f8
Uppalapati Poojitha <poojitha3030@gmail.com>
2026-06-06 13:37:58 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,9 +6,14 @@
 // You can pass additional config via defineConfig({ vite: { ... }, etc... }) if needed.
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
+
+
 export default defineConfig({
-  server: {
-    allowedHosts: true,
+  vite: {
+    server: {
+      host: "0.0.0.0",
+      allowedHosts: true,
+    },
   },
 
   tanstackStart: {
